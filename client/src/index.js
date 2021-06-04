66798d0356480712544690639dcb680e7b7b8f7b
Adam Marsala <adammarsala@hotmail.com>
2021-06-04 13:40:45 -0400

    Add game assets folder to src client and remove stylesheets link

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 // import Phaser from "phaser";
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import App from "./components/App.jsx";
+// // import React from "react";
+// // import ReactDOM from "react-dom";
+// // import App from "./components/App.jsx";
 // import Forest from "./phaser/forestScene";
 // import Town from "./phaser/overworld";
 // import Dungeon from "./phaser/DungeonScene";
