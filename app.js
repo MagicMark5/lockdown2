3ff48a71d37017c6dc1d27ed1f0050050bd3075d
Adam Marsala <adammarsala@hotmail.com>
2021-06-10 14:06:03 -0400

    Remove boilerplate code for express and webpack build

--- app.js
+++ app.js
@@ -14,13 +14,17 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'src')));
 
+// The directory to serve is 'dist' after we run 'npm run build'
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Define http request routes here
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter(dbHelpers));
 
+// Catch-all to serve index.html for any undefined routes 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 module.exports = app;
