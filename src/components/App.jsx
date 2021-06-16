510c32fd276e44921459365a6618c5ccdbabe8e5
Adam Marsala <adammarsala@hotmail.com>
2021-06-16 11:25:44 -0400

    Add axios to dependencies, make get call to retrieve highscores from app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 import Controls from './Controls.jsx';
 import GameStats from "./GameStats.jsx";
 import Highscores from "./Highscores.jsx";
@@ -6,6 +7,23 @@ import Navigation from "./Navigation.jsx";
 import LoginNav from "./LoginNav.jsx";
 
 export default function App(props) {
+	const [gameSession, setGameSession] = useState(null);
+
+	// retrieve highscores from db on first render
+	// update highscores when post request to insert a game session into db is successful
+	// then setGameSession will update gameSession state with new game session
+  useEffect(() => {
+    if (gameSession) {
+      // post query to shoppies-backend on App re-render / when the query changes
+      axios.get(`api/users/highscores`)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(e => {
+        console.log(e);
+      })
+    }
+  }, [gameSession])
 
 	return (
 		<>
