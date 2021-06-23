d04afcce68289026751f96272f446028f3ed35ac
Adam Marsala <adammarsala@hotmail.com>
2021-06-23 13:38:03 -0400

    Condition of which db connection string to use for production vs development

--- db/index.js
+++ db/index.js
@@ -3,14 +3,21 @@ require('dotenv').config();
 
 const env = process.env.NODE_ENV;
 
-const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable` ;
+production
+
+const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
+const dbConnection = env === 'development' ? connectionString : process.env.DATABASE_URL
 
 const client = new pg.Client({
-    connectionString: connectionString || process.env.DATABASE_URL,
+    connectionString: dbConnection,
 });
 
-console.log( `Connected to ${process.env.DB_NAME} on ${process.env.DB_HOST}` );
-console.log( `Connected to ${process.env.DATABASE_URL} on ${env} mode` );
+if (env === 'development') {
+    console.log( `Connected to ${process.env.DB_NAME} on ${process.env.DB_HOST}` );
+} else if (env === 'production') {
+    console.log( `Connected to ${process.env.DATABASE_URL} on ${env} mode` );
+}
+
 client.connect();
 
 module.exports = client;
