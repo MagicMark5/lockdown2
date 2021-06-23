20f2601d6e23ca692cc7503623c2b0a1f25337a2
Adam Marsala <adammarsala@hotmail.com>
2021-06-23 13:48:44 -0400

    Fix error in db index

--- db/index.js
+++ db/index.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 
 const env = process.env.NODE_ENV;
 
-production
-
 const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
 const dbConnection = env === 'development' ? connectionString : process.env.DATABASE_URL
 
